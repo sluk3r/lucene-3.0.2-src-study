ea418c8fa18563e031d784773e39e4155a9defae
sluk3r <sluk3r@gmail.com>
2014-04-16 09:15:40 +0000

    TestSearch文件， 对PhraseQuery有了个初步的感觉。 省得自己写相应的例子

--- src/test/org/apache/lucene/TestSearch.java
+++ src/test/org/apache/lucene/TestSearch.java
@@ -57,16 +57,21 @@ public class TestSearch extends LuceneTestCase {
       pw.close();
       sw.close();
       String multiFileOutput = sw.getBuffer().toString();
-      //System.out.println(multiFileOutput);
+      System.out.println("multiFileOutput:\n" + multiFileOutput);
 
-      sw = new StringWriter();
-      pw = new PrintWriter(sw, true);
-      doTestSearch(pw, true);
-      pw.close();
-      sw.close();
-      String singleFileOutput = sw.getBuffer().toString();
+//      sw = new StringWriter();
+//      pw = new PrintWriter(sw, true);
+//      doTestSearch(pw, true);
+//      pw.close();
+//      sw.close();
+//      String singleFileOutput = sw.getBuffer().toString();
+//
+//      assertEquals(multiFileOutput, singleFileOutput);
+//
+//      System.out.println("========================================================");
+//      System.out.println("singleFileOutput: \n" + singleFileOutput);
 
-      assertEquals(multiFileOutput, singleFileOutput);
+        //wangxc question 从结果没看出来是singleFileOutput与否的区别
     }
 
 
@@ -87,13 +92,17 @@ public class TestSearch extends LuceneTestCase {
         "a c e",
         "e c a",
         "a c e a c e",
-        "a c e a b c"
+        "a c e a b c",
+        "a c e a f b c",//wangxc 新加了这个， 想试下slop的影响。 从结果看， 这个slop是4时，还能查询出来“a c e a f b c”， 感觉这里的slop已经大于4了。 再加一个试试？
+        "a c e a f g h i j k b c"//wangxc, 看到效果了，这里a与b之间距离远大于4了， 结果没查询出来。  在Phrase的专门测试里应该有更精确的表示。
       };
       for (int j = 0; j < docs.length; j++) {
         Document d = new Document();
         d.add(new Field("contents", docs[j], Field.Store.YES, Field.Index.ANALYZED));
         writer.addDocument(d);
       }
+
+      out.println("docs indexed  size:" + docs.length);
       writer.close();
 
       Searcher searcher = new IndexSearcher(directory, true);
@@ -104,15 +113,16 @@ public class TestSearch extends LuceneTestCase {
         "\"a b c\"",
         "a c",
         "\"a c\"",
-        "\"a c e\"",
+        "\"a c e\"", //wangxc 这几种是针对Phrase的测试？
       };
+      //wangxc 看了测试打印出来的结果， 发现这个正是理解Phrase的好材料
       ScoreDoc[] hits = null;
 
       QueryParser parser = new QueryParser(Version.LUCENE_CURRENT, "contents", analyzer);
-      parser.setPhraseSlop(4);
+      parser.setPhraseSlop(4); //wangxc 这个4是小于等于4的意思？
       for (int j = 0; j < queries.length; j++) {
         Query query = parser.parse(queries[j]);
-        out.println("Query: " + query.toString("contents"));
+        out.println("\n\n\n Query: " + query.toString("contents")); //wangxc
 
       //DateFilter filter =
       //  new DateFilter("modified", Time(1997,0,1), Time(1998,0,1));
@@ -129,6 +139,7 @@ public class TestSearch extends LuceneTestCase {
                              + " " + d.get("contents"));
         }
       }
+
       searcher.close();
   }
 
@@ -138,3 +149,215 @@ public class TestSearch extends LuceneTestCase {
     return calendar.getTime().getTime();
   }
 }
+
+
+/**
+ *
+
+
+ docs indexed  size:9
+
+
+
+ Query: a b
+ 9 total results
+ 0 0.67981035 a b c d e a b c d e
+ 1 0.6729779 a b c d e
+ 2 0.6576601 a c e a b c
+ 3 0.6576601 a c e a f b c
+ 4 0.48069853 a b c d e f g h i j
+ 5 0.43844008 a c e a f g h i j k b c
+ 6 0.1379716 a c e a c e
+ 7 0.13008086 a c e
+ 8 0.13008086 e c a
+
+
+
+ Query: "a b"~4
+ 5 total results
+ 0 0.94838655 a b c d e a b c d e
+ 1 0.9388548 a b c d e
+ 2 0.8047327 a c e a b c
+ 3 0.6706106 a b c d e f g h i j
+ 4 0.56903195 a c e a f b c
+
+
+
+ Query: "a b c"~4
+ 5 total results
+ 0 1.3437651 a b c d e a b c d e
+ 1 1.3302596 a b c d e
+ 2 1.2490513 a c e a b c
+ 3 0.9501854 a b c d e f g h i j
+ 4 0.80625904 a c e a f b c
+
+
+
+ Query: a c
+ 9 total results
+ 0 0.67097956 a c e a c e
+ 1 0.67097956 a c e a b c
+ 2 0.67097956 a c e a f b c
+ 3 0.6326056 a c e
+ 4 0.6326056 e c a
+ 5 0.5591496 a b c d e a b c d e
+ 6 0.5535299 a b c d e
+ 7 0.44731972 a c e a f g h i j k b c
+ 8 0.3953785 a b c d e f g h i j
+
+
+
+ Query: "a c"~4
+ 9 total results
+ 0 0.94890845 a c e a c e
+ 1 0.8946395 a c e
+ 2 0.88762254 a c e a b c
+ 3 0.8442975 a c e a f b c
+ 4 0.61251783 a b c d e a b c d e
+ 5 0.5535299 a b c d e
+ 6 0.5165203 e c a //wangxc 这个反而有高分了？ 背后的原理？
+ 7 0.5001187 a c e a f g h i j k b c //wangxc 这个为什么没有高分？ 是因为后面还有离的很远的a和c？
+ 8 0.3953785 a b c d e f g h i j //wangxc 这个分低是因为文档长？a和c之间又没有紧挨着？
+
+
+
+ Query: "a c e"~4
+ 9 total results
+ 0 1.5097042 a c e a c e //wangxc 在Lucene内部是怎么区分连续着两个ace的？   第一次见打分高于1的！！！
+ 1 1.3419592 a c e
+ 2 1.2119497 a c e a b c
+ 3 1.125267 a c e a f b c
+ 4 0.86623096 a b c d e a b c d e
+ 5 0.75017804 a c e a f g h i j k b c
+ 6 0.677933 a b c d e
+ 7 0.6001424 e c a
+ 8 0.48423782 a b c d e f g h i j
+ *
+ */
+
+
+
+
+
+/**
+multiFileOutput:
+Query: a b
+9 total results
+0 0.67981035 a b c d e a b c d e
+1 0.6729779 a b c d e
+2 0.6576601 a c e a b c
+3 0.6576601 a c e a f b c
+4 0.48069853 a b c d e f g h i j
+5 0.43844008 a c e a f g h i j k b c
+6 0.1379716 a c e a c e
+7 0.13008086 a c e
+8 0.13008086 e c a
+Query: "a b"~4
+5 total results
+0 0.94838655 a b c d e a b c d e
+1 0.9388548 a b c d e
+2 0.8047327 a c e a b c
+3 0.6706106 a b c d e f g h i j
+4 0.56903195 a c e a f b c
+Query: "a b c"~4
+5 total results
+0 1.3437651 a b c d e a b c d e
+1 1.3302596 a b c d e
+2 1.2490513 a c e a b c
+3 0.9501854 a b c d e f g h i j
+4 0.80625904 a c e a f b c
+Query: a c
+9 total results
+0 0.67097956 a c e a c e
+1 0.67097956 a c e a b c
+2 0.67097956 a c e a f b c
+3 0.6326056 a c e
+4 0.6326056 e c a
+5 0.5591496 a b c d e a b c d e
+6 0.5535299 a b c d e
+7 0.44731972 a c e a f g h i j k b c
+8 0.3953785 a b c d e f g h i j
+Query: "a c"~4
+9 total results
+0 0.94890845 a c e a c e
+1 0.8946395 a c e
+2 0.88762254 a c e a b c
+3 0.8442975 a c e a f b c
+4 0.61251783 a b c d e a b c d e
+5 0.5535299 a b c d e
+6 0.5165203 e c a
+7 0.5001187 a c e a f g h i j k b c
+8 0.3953785 a b c d e f g h i j
+Query: "a c e"~4
+9 total results
+0 1.5097042 a c e a c e
+1 1.3419592 a c e
+2 1.2119497 a c e a b c
+3 1.125267 a c e a f b c
+4 0.86623096 a b c d e a b c d e
+5 0.75017804 a c e a f g h i j k b c
+6 0.677933 a b c d e
+7 0.6001424 e c a
+8 0.48423782 a b c d e f g h i j
+
+========================================================
+singleFileOutput:
+Query: a b
+9 total results
+0 0.67981035 a b c d e a b c d e
+1 0.6729779 a b c d e
+2 0.6576601 a c e a b c
+3 0.6576601 a c e a f b c
+4 0.48069853 a b c d e f g h i j
+5 0.43844008 a c e a f g h i j k b c
+6 0.1379716 a c e a c e
+7 0.13008086 a c e
+8 0.13008086 e c a
+Query: "a b"~4
+5 total results
+0 0.94838655 a b c d e a b c d e
+1 0.9388548 a b c d e
+2 0.8047327 a c e a b c
+3 0.6706106 a b c d e f g h i j
+4 0.56903195 a c e a f b c
+Query: "a b c"~4
+5 total results
+0 1.3437651 a b c d e a b c d e
+1 1.3302596 a b c d e
+2 1.2490513 a c e a b c
+3 0.9501854 a b c d e f g h i j
+4 0.80625904 a c e a f b c
+Query: a c
+9 total results
+0 0.67097956 a c e a c e
+1 0.67097956 a c e a b c
+2 0.67097956 a c e a f b c
+3 0.6326056 a c e
+4 0.6326056 e c a
+5 0.5591496 a b c d e a b c d e
+6 0.5535299 a b c d e
+7 0.44731972 a c e a f g h i j k b c
+8 0.3953785 a b c d e f g h i j
+Query: "a c"~4
+9 total results
+0 0.94890845 a c e a c e
+1 0.8946395 a c e
+2 0.88762254 a c e a b c
+3 0.8442975 a c e a f b c
+4 0.61251783 a b c d e a b c d e
+5 0.5535299 a b c d e
+6 0.5165203 e c a
+7 0.5001187 a c e a f g h i j k b c
+8 0.3953785 a b c d e f g h i j
+Query: "a c e"~4
+9 total results
+0 1.5097042 a c e a c e
+1 1.3419592 a c e
+2 1.2119497 a c e a b c
+3 1.125267 a c e a f b c
+4 0.86623096 a b c d e a b c d e
+5 0.75017804 a c e a f g h i j k b c
+6 0.677933 a b c d e
+7 0.6001424 e c a
+8 0.48423782 a b c d e f g h i j
+ */
